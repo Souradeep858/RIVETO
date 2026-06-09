02743dea4d65618de6ba3a63b2ab86593e4e33e8
Sharayu Shelke <sharayushelke2006@gmail.com>
2026-06-09 20:01:14 +0530

    fixed footer missing recommendations

--- frontend/src/pages/Recommendations.jsx
+++ frontend/src/pages/Recommendations.jsx
@@ -1,5 +1,11 @@
 import IssueRecommendations from '../components/IssueRecommendations';
+import Footer from '../components/Footer';
 
 export default function Recommendations() {
-  return <IssueRecommendations />;
+  return (
+    <>
+      <IssueRecommendations />
+      <Footer />
+    </>
+  );
 }
